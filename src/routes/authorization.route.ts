b74fb1b5b1cd4e18a58ea62fbb2fecab1f2d875c
Nayuta <naayudan@gmail.com>
2022-05-12 08:57:36 -0300

    Basic Authentication

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import ForbiddenError from '../models/errors/forbidden.error.model';
 import userRepository from '../repositories/user.repository';
+import JWT from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
 
 const authorizationRoute = Router();
 
@@ -12,23 +14,32 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next: Next
             throw new ForbiddenError('Credenciais não informadas');
         }
 
-        const [authenticationType, token] =  authorizationHeader.split(' ');
+        const [authenticationType, token] = authorizationHeader.split(' ');
 
-        if (authenticationType !== 'Basic' || !token){
+        if (authenticationType !== 'Basic' || !token) {
             throw new ForbiddenError('Tipo de Autenticação Inválido')
         }
 
         const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
         const [username, password] = tokenContent.split(':');
 
-        if (!username || !password){
+        if (!username || !password) {
             throw new ForbiddenError('Credenciais não preenchidas')
         }
 
         const user = await userRepository.findByUsernameAndPassword(username, password);
-        console.log(user)
 
-        
+        if (!user) {
+            throw new ForbiddenError('Usuário ou senha inválidos!')
+        }
+
+        const jwtPayload = { username: user.username };
+        const jwtOptions = { subject: user?.uuid }
+        const secretKey = 'my_secrect_key'
+
+        const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
+
+        res.status(StatusCodes.OK).json({ token: jwt })
 
     } catch (error) {
         next(error)
