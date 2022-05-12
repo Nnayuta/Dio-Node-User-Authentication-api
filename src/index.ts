f72dc0667852cdda45abb79c565a6d6fbc337a1f
Nayuta <naayudan@gmail.com>
2022-05-12 08:49:01 -0300

    basic authentication

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import errorHandle from './middlewares/error-handler.middleware';
+import authorizationRoute from './routes/authorization.route';
 import statusRouter from './routes/status.route';
 import userRoute from './routes/users.route';
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }))
 // Configurações de Rotas
 app.use(userRoute)
 app.use(statusRouter)
+app.use(authorizationRoute)
 
 // Configuração dos Handlers de Erro
 
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -29,11 +29,33 @@ class UserRepository {
             const [user] = rows
 
             return user;
-        }catch(error){
+        } catch (error) {
             throw new DatabaseError("Erro na consulta por ID", error)
         }
 
     }
+
+    // FIND USER BY USER NAME AND PASSWORD
+
+    async findByUsernameAndPassword(username: string, password: string): Promise<User | null> {
+        try {
+            const query = `
+                SELECT uuid, username
+                FROM application_user
+                WHERE username = $1
+                AND password = crypt($2, 'my_salt')
+            `
+
+            const values = [username, password]
+            const { rows } = await db.query<User>(query, values)
+            const [user] = rows;
+            return user || null;
+        } catch (error) {
+            throw new DatabaseError('Erro na consulta por username e password', error)
+        }
+    }
+
+    /// CREATE NEW USER
 
     async create(user: User): Promise<string> {
         const script = `
